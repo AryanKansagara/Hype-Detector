8b23c50219865c565ea06cd37c5812c3727b4c93
Aryan Kansagara <aryankk07@gmail.com>
2025-12-18 09:41:18 -0500

    changed the title bar create next app-> hype detector

--- frontend/.next/dev/static/chunks/e40d0_next_dist_client_components_builtin_global-error_7a1822fd.js
+++ frontend/.next/dev/static/chunks/e40d0_next_dist_client_components_builtin_global-error_7a1822fd.js
@@ -1,5 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [],
+    chunks: [
+  "static/chunks/e40d0_next_dist_ec24cdab._.js"
+],
     source: "dynamic"
 });
